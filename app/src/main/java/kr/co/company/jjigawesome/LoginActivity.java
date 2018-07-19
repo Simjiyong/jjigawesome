4a54d9785f227348889b0dab87186240def1ce1d
Simjiyong <sjyjoa@gmail.com>
2018-07-19 14:53:57 +0900

    a

--- app/src/main/java/kr/co/company/jjigawesome/LoginActivity.java
+++ app/src/main/java/kr/co/company/jjigawesome/LoginActivity.java
@@ -1,8 +1,8 @@
 package kr.co.company.jjigawesome;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
