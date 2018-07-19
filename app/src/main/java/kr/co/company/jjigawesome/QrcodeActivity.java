b8515419e4707ce6f4776fcaae877d641b79a6fd
BHY <inomp3042@naver.com>
2018-07-19 17:29:27 +0900

    nonfullscreen

--- app/src/main/java/kr/co/company/jjigawesome/QrcodeActivity.java
+++ app/src/main/java/kr/co/company/jjigawesome/QrcodeActivity.java
@@ -26,12 +26,12 @@ public class QrcodeActivity extends AppCompatActivity {
             Log.i("Is on?", "Turning immersive mode mode on.");
         }
 // 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
-        newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-        newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
 
         view.setSystemUiVisibility(newUiOptions);
         view.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
--- app/src/main/java/kr/co/company/jjigawesome/SignUpActivity.java
+++ app/src/main/java/kr/co/company/jjigawesome/SignUpActivity.java
@@ -35,12 +35,12 @@ public class SignUpActivity extends AppCompatActivity {
             Log.i("Is on?", "Turning immersive mode mode on.");
         }
 // 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
-        newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-        newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
 
         view.setSystemUiVisibility(newUiOptions);
         view.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
@@ -74,8 +74,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         Log.d("ddd", ""+(int)((113*((metrics.densityDpi/160))*(float)height/1920)));
 
-        Intent intent = new Intent(SignUpActivity.this, CompleteQrcodeActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(SignUpActivity.this, CompleteQrcodeActivity.class);
+        //startActivity(intent);
     }
 
     public int getStatusBarHeight()
