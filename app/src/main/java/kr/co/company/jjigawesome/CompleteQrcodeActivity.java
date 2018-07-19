478201b1ffb21b775b37ea30c194f8d08e1d53f2
BHY <inomp3042@naver.com>
2018-07-19 17:41:17 +0900

    full(16:9) + nonfull

--- app/src/main/java/kr/co/company/jjigawesome/CompleteQrcodeActivity.java
+++ app/src/main/java/kr/co/company/jjigawesome/CompleteQrcodeActivity.java
@@ -1,9 +1,14 @@
 package kr.co.company.jjigawesome;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+
+import java.lang.reflect.Method;
 
 public class CompleteQrcodeActivity extends AppCompatActivity {
 
@@ -15,30 +20,62 @@ public class CompleteQrcodeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_qrcode);
 
-        view = getWindow().getDecorView();
-        int uiOptions = view.getSystemUiVisibility();
-        newUiOptions = uiOptions;
-        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
-        if (isImmersiveModeEnabled) {
-            Log.i("Is on?", "Turning immersive mode mode off. ");
-        } else {
-            Log.i("Is on?", "Turning immersive mode mode on.");
-        }
-// 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
-        //newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+        Display display = this.getWindowManager().getDefaultDisplay();
+        int realWidth;
+        int realHeight;
 
-        view.setSystemUiVisibility(newUiOptions);
-        view.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                view.setSystemUiVisibility(newUiOptions);
+        if (Build.VERSION.SDK_INT >= 17){
+            //new pleasant way to get real metrics
+            DisplayMetrics realMetrics = new DisplayMetrics();
+            display.getRealMetrics(realMetrics);
+            realWidth = realMetrics.widthPixels;
+            realHeight = realMetrics.heightPixels;
+
+        } else if (Build.VERSION.SDK_INT >= 14) {
+            //reflection for this weird in-between time
+            try {
+                Method mGetRawH = Display.class.getMethod("getRawHeight");
+                Method mGetRawW = Display.class.getMethod("getRawWidth");
+                realWidth = (Integer) mGetRawW.invoke(display);
+                realHeight = (Integer) mGetRawH.invoke(display);
+            } catch (Exception e) {
+                //this may not be 100% accurate, but it's all we've got
+                realWidth = display.getWidth();
+                realHeight = display.getHeight();
+                Log.e("Display Info", "Couldn't use reflection to get the real display metrics.");
             }
-        });
+
+        } else {
+            //This should be close, as lower API devices should not have window navigation bars
+            realWidth = display.getWidth();
+            realHeight = display.getHeight();
+        }
+
+        if(realWidth * 16 == realHeight * 9){
+            view = getWindow().getDecorView();
+            int uiOptions = view.getSystemUiVisibility();
+            newUiOptions = uiOptions;
+            boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+            if (isImmersiveModeEnabled) {
+                Log.i("Is on?", "Turning immersive mode mode off. ");
+            } else {
+                Log.i("Is on?", "Turning immersive mode mode on.");
+            }
+            newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+            newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+            newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+
+            view.setSystemUiVisibility(newUiOptions);
+            view.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    view.setSystemUiVisibility(newUiOptions);
+                }
+            });
+        }
 
 
     }
--- app/src/main/java/kr/co/company/jjigawesome/LoginActivity.java
+++ app/src/main/java/kr/co/company/jjigawesome/LoginActivity.java
@@ -1,12 +1,17 @@
 package kr.co.company.jjigawesome;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import java.lang.reflect.Method;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,29 +25,62 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        view = getWindow().getDecorView();
-        int uiOptions = view.getSystemUiVisibility();
-        newUiOptions = uiOptions;
-        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
-        if (isImmersiveModeEnabled) {
-            Log.i("Is on?", "Turning immersive mode mode off. ");
-        } else {
-            Log.i("Is on?", "Turning immersive mode mode on.");
-        }
-// 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
-        //newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        //newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
-        view.setSystemUiVisibility(newUiOptions);
-        view.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                view.setSystemUiVisibility(newUiOptions);
+        Display display = this.getWindowManager().getDefaultDisplay();
+        int realWidth;
+        int realHeight;
+
+        if (Build.VERSION.SDK_INT >= 17){
+            //new pleasant way to get real metrics
+            DisplayMetrics realMetrics = new DisplayMetrics();
+            display.getRealMetrics(realMetrics);
+            realWidth = realMetrics.widthPixels;
+            realHeight = realMetrics.heightPixels;
+
+        } else if (Build.VERSION.SDK_INT >= 14) {
+            //reflection for this weird in-between time
+            try {
+                Method mGetRawH = Display.class.getMethod("getRawHeight");
+                Method mGetRawW = Display.class.getMethod("getRawWidth");
+                realWidth = (Integer) mGetRawW.invoke(display);
+                realHeight = (Integer) mGetRawH.invoke(display);
+            } catch (Exception e) {
+                //this may not be 100% accurate, but it's all we've got
+                realWidth = display.getWidth();
+                realHeight = display.getHeight();
+                Log.e("Display Info", "Couldn't use reflection to get the real display metrics.");
             }
-        });
+
+        } else {
+            //This should be close, as lower API devices should not have window navigation bars
+            realWidth = display.getWidth();
+            realHeight = display.getHeight();
+        }
+
+        if(realWidth * 16 == realHeight * 9){
+            view = getWindow().getDecorView();
+            int uiOptions = view.getSystemUiVisibility();
+            newUiOptions = uiOptions;
+            boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+            if (isImmersiveModeEnabled) {
+                Log.i("Is on?", "Turning immersive mode mode off. ");
+            } else {
+                Log.i("Is on?", "Turning immersive mode mode on.");
+            }
+            newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+            newUiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+            newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+
+            view.setSystemUiVisibility(newUiOptions);
+            view.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    view.setSystemUiVisibility(newUiOptions);
+                }
+            });
+        }
 
         button_signup = (Button) findViewById(R.id.button_login_signup);
         button_signup.setOnClickListener(new View.OnClickListener() {
